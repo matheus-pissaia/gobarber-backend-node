662ba20493810159a8fa983ca711cf464e624e73
Matheus Pissaia <69802406+matheus-pissaia@users.noreply.github.com>
2021-01-20 00:28:23 -0300

    Update File IHashProvider

--- src/modules/users/providers/HashProvider/models/IHashProvider.ts
+++ src/modules/users/providers/HashProvider/models/IHashProvider.ts
@@ -1,5 +1,3 @@
-import { de } from 'date-fns/locale';
-
 export default interface IHashProvider {
   generateHash(payload: string): Promise<string>;
   compareHash(payload: string, hashed: string): Promise<boolean>;
